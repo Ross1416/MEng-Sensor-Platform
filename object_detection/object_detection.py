14234169f7a3097a6084f8a326d935f070d2eb47
Ross Inglis <r.inglis617@gmail.com>
2025-02-12 14:54:58 +0000

    Created pixel_to_angle function

--- object_detection/object_detection.py
+++ object_detection/object_detection.py
@@ -13,6 +13,25 @@ def object_detection(model, frame):
         results.append([label,coords,conf])
     return results
 
+def pixel_to_angle(pixel,res,fov):
+    """Takes in pixel and returns angle from centre. Assumes linear FOV.
+    pixel: (x,y) pixel location to caluclate angle to  
+    res: (xres,yres) resolution of image
+    fov: (xfov,yfov) field of view of camera"""
+
+    x_angle_res = fov[0]/res[0]
+    y_angle_res = fov[1]/res[1]
+
+    centre_x = int(res[0]/2)
+    centre_y = int(res[1]/2)
+
+    x_pixel_change = pixel[0] - centre_x
+    y_pixel_change = pixel[1] - centre_y
+
+    x_angle = x_pixel_change * x_angle_res
+    y_angle = y_pixel_change * y_angle_res
+
+    return (x_angle,y_angle)
 
 if __name__ == "__main__":
     import cv2
@@ -21,3 +40,5 @@ if __name__ == "__main__":
     img = cv2.imread("dog.jpg")
     results = object_detection(model,img)
     print(results)
+
+    print(pixel_to_angle((4600,0),(4608,2592),(102,67)))
