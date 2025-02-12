036f47dbd6fcc3c978d946416756cba336ee9ed7
Ross Inglis <r.inglis617@gmail.com>
2025-02-12 13:45:12 +0000

    Added function to wait for change in location

--- gps/gps.py
+++ gps/gps.py
@@ -1,13 +1,17 @@
 import serial
 import pynmea2
-
+import math
+import time
 
 class Neo6M:
-    def __init__(self, port="/dev/ttyAMA0", baudrate=9600, timeout=1):
+    def __init__(self, port="/dev/ttyAMA0", baudrate=9600, timeout=1, distance_threshold=10, movement_callback=None):
         self.port = port
         self.baudrate = baudrate
         self.timeout = timeout
         self.connection = serial.Serial(port, baudrate, timeout=timeout)
+        self.distance_threshold = distance_threshold
+        self.last_location = (0,0)
+        self.movement_callback = movement_callback
 
     def read_raw_data(self):
         """Reads raw NMEA sentence from the GPS module. Returns Raw NMEA sentence as a string or None if no data is available."""
@@ -22,8 +26,7 @@ class Neo6M:
 
     def get_location(self):
         """Reads and parses the GPS location from NMEA sentences. Returns Dictionary containing latitude, longitude, altitude, and fix quality or None if no fix."""
-        # TODO include timeout incase serial error 
-        # TODO return error if no GPS fix?
+        count = 0
         while True:
             raw_data = self.read_raw_data()
             if raw_data and raw_data.startswith("$GPGGA"):  # GGA contains location data
@@ -38,6 +41,40 @@ class Neo6M:
                         }
                 except pynmea2.ParseError:
                     pass
+                    
+            if count > 20:
+                return None
+            else:
+                count += 1
+    
+    def haversine(self, pos1, pos2):
+        """Calculates distance in meters between two GPS coordinates.
+        pos1 = (lat1,lon1)
+        pos2 = (lat2,lon2)"""
+        lat1, lon1 = pos1
+        lat2, lon2 = pos2
+        R = 6371000  # Earth radius in meters
+        phi1, phi2 = math.radians(lat1), math.radians(lat2)
+        delta_lat = math.radians(lat2 - lat1)
+        delta_lon = math.radians(lon2 - lon1)
+
+        a = (math.sin(delta_lat / 2) ** 2) + math.cos(phi1) * math.cos(phi2) * (math.sin(delta_lon / 2) ** 2)
+        c = 2 * math.asin(math.sqrt(a))
+
+        return R * c  # Distance in meters
+
+    def wait_for_movement(self):
+        """Holds until distance moved > {distance_threshold} and triggers callback function if provided and exits."""
+        while distance_moved < self.distance_threshold:
+            location = self.get_location()
+            if location:
+                distance_moved = self.haversine(location, self.last_location)
+                
+                if distance_moved >= self.distance_threshold:
+                    if self.movement_callback:
+                        self.movement_callback(location)
+                
+            time.sleep(1)
 
     def close(self):
         """Closes the serial connection."""
