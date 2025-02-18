0033da076a3549b51bafffa34b15287cc82f1e32
Ross <r.inglis617@gmail.com>
2025-02-18 16:23:53 +0000

    Removed saving of images and fixed compression

--- PiA.py
+++ PiA.py
@@ -11,7 +11,7 @@ from stitching.stitching_main import performPanoramicStitching
 # Triggers when change in GPS location
 def new_scan(rgb_model, lon=0.00, lat=0.00):
     # Captures 2 images
-    frames = capture(cams, "PiA", save_location)
+    frames = capture(cams, "PiA")
     # Triggers capture on PiB
     request_client_capture(server_socket, conn)
     # Perform object detection
@@ -20,18 +20,13 @@ def new_scan(rgb_model, lon=0.00, lat=0.00):
         objects.append(object_detection(rgb_model,f))
     # Retrieve slave images and data
     # receive_images(save_location, server_socket, conn)  
-    b_frames = receive_image_arrays(conn)
-    print(f"length of frames before {len(frames)}")
-    frames.append(b_frames)
-    print(f"length of frames after {len(frames)}")
+    frames += receive_image_arrays(conn)
 
     # send rotational stage control signal
     angle_x, _ = pixel_to_angle((50,100),RESOLUTION,FOV)
     # Perform pano stitching
     # TO DO: clean this up
-    image3 = cv2.imread(save_location+'PiB_0.jpg')
-    image4 = cv2.imread(save_location+'PiB_1.jpg')
-    panorama = performPanoramicStitching(frames[0], frames[1], image3, image4)
+    panorama = performPanoramicStitching(frames[0], frames[1], frames[2], frames[3])
     # Receive hsi photo and data 
     # Updates json and moves images to correct folder
     uid = str(lon)+str(lat)
