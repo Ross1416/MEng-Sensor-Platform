c4582eebcbfa478247a294d2497355f20b91c6e6
Ross Inglis <r.inglis617@gmail.com>
2025-02-24 18:19:48 +0000

    Added rotation stage to integrated scripts

--- PiB.py
+++ PiB.py
@@ -4,8 +4,9 @@ from comms.receive import *
 from cameras import *
 from time import sleep
 import logging
+from hyperspectral.zaber_driver import *
 
-def on_trigger(rgb_model):
+def on_trigger(rgb_model,axis):
     # Capture images
     frames = capture(cams, "PiB", PATH)
     # Perform object detection
@@ -20,10 +21,17 @@ def on_trigger(rgb_model):
     send_image_arrays(client_socket,frames)
     # Send object detection results to PiA
     send_object_detection_results(client_socket, objects[2:])  
+    # Take hyperspectral scan - test for 1st object
+    px_1 = results[0][0][1][:2]
+    px_2 = results[0][0][1][2:]
+    angle_x1, _ = pixel_to_angle(px_1,RESOLUTION,FOV)
+    angle_x2, _ = pixel_to_angle(px_2,RESOLUTION,FOV)
+    on_rotate(axis,(angle_x1,angle_x2))
     
 
-def on_rotate():
+def on_rotate(axis,angles):
     # Rotate rotational stage 
+    axis.move_absolute(angles[0],Units.ANGLE_DEGREES,velocity=40,velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,wait_until_idle=True)
     # take hsi image
     # process hsi image
     # return hsi colour image and data
@@ -34,7 +42,11 @@ def on_rotate():
 IP = "hsiA.local"
 PORT = 5002
 PATH = "./captures/"
-CLASSES = ["person"] 
+CLASSES = ["person","face"] 
+ROTATIONAL_STAGE_PORT = "/dev/ttyUSB0" # TODO: find automatically?
+
+RESOLUTION = (4608,2592)
+FOV = (102,67)
 
 if __name__ == "__main__":
     # Setup Logging
@@ -50,6 +62,10 @@ if __name__ == "__main__":
         # Setup cameras and capture images
         cams = setup_cameras()
         logging.debug("Setup cameras.")
+        # Setup rotational stage
+        conn, axis = setup_zaber(ROTATIONAL_STAGE_PORT)
+        # Home rotational stage
+        axis.home(wait_until_idle=False)
         # Make connection with PiA
         client_socket = make_client_connection(IP, PORT)
         logging.debug("Connected to PiA")
@@ -65,7 +81,7 @@ if __name__ == "__main__":
         while True:
             if receive_capture_request(client_socket) == 1:
                 logging.info("Triggered Capture.")
-                on_trigger(rgb_model)
+                on_trigger(rgb_model,axis)
                 capture_triggered = True
             
             sleep(1)
