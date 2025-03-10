40b9fc2fd34abf81ece3ee0ab0f8f6dad7122be8
SeanDoherty <sean.doherty.2020@uni.strath.ac.uk>
2025-03-10 15:39:10 +0000

    Finalised stitching function

--- stitching/stitching_main.py
+++ stitching/stitching_main.py
@@ -2,65 +2,49 @@ from stitching_functions import *
 import cv2
 import numpy as np
 
-def performPanoramicStitching(image1, image2, image3, image4, defaultAffine=defaultAffineTransform()):
+def performPanoramicStitching(image1, image2, image3, image4):
     image1 = cylindricalProjection(image1)
     image2 = cylindricalProjection(image2)
     image3 = cylindricalProjection(image3)
     image4 = cylindricalProjection(image4)
 
-    # FIRST STITCH
-    # Find Key Points & Calculate Transform
-    src_pts, dst_pts = findKeyPoints(image1, image2)
-    calculatedAffine = calculateTransform(dst_pts, src_pts)
+    # Commented code below is used for calibration
 
-    # Compare default and calcaulted transform
-    frobenius_norm = np.linalg.norm(calculatedAffine - defaultAffine, 'fro')
-    
-    # Use the most suitable transform
-    # if frobenius_norm < 300:
-    if True:
-        panorama = applyTransform(image1, image2, calculatedAffine)
-    else:
-        panorama = applyTransform(image1, image2, defaultAffine)
+    ### First stitch ###
+    # src_pts, dst_pts = findKeyPoints(image1, image2)
+    # H1 = calculateTransform(dst_pts, src_pts)
+    # [1.16777650e+00  3.24501623e-02  3.20424227e+03], [-4.82860118e-02  1.00046217e+00  8.67739018e+00]
+    # H1 = np.array([[1.16777650e+00,  3.24501623e-02,  3.20424227e+03], [-4.82860118e-02,  1.00046217e+00,  8.67739018e+00]])
+    H1 = np.array([[1.16777650e+00,  3.24501623e-02,  3.20424227e+03], [0,  1,  0]])
+    panorama = applyTransform(image1, image2, H1)
 
-    # SECOND STITCH
-    # Find Key Points & Calculate Transform
-    src_pts, dst_pts = findKeyPoints(panorama, image3)
-    calculatedAffine = calculateTransform(dst_pts, src_pts)
+    ### Second Stitch ###
+    # src_pts, dst_pts = findKeyPoints(panorama, image3)
+    # H2 = calculateTransform(dst_pts, src_pts)
+    # [9.55467955e-01  1.08327684e-01  7.00375956e+03], [-7.86960457e-02  9.88385699e-01 -1.73228273e+02]
+    # H2 = np.array([[9.55467955e-01,  1.08327684e-01,  7.00375956e+03], [-7.86960457e-02,  9.88385699e-01, -1.73228273e+02]])
+    H2 = np.array([[9.55467955e-01,  1.08327684e-01,  7.00375956e+03], [0,  1,  0]])
+    panorama = applyTransform(panorama, image3, H2)
 
-    # Compare default and calculated transform
-    frobenius_norm = np.linalg.norm(calculatedAffine - defaultAffine, 'fro')
-    
-    # Use the most suitable transform
-    # if frobenius_norm < 300:
-    if True:
-        panorama = applyTransform(panorama, image3, calculatedAffine)
-    else:
-        panorama = applyTransform(panorama, image3, defaultAffine)
+    ### Third Stitch ###
+    # src_pts, dst_pts = findKeyPoints(panorama, image4)
+    # H3 = calculateTransform(dst_pts, src_pts)
+    # [ 8.66225987e-01  1.23513301e-01  1.00071255e+04], [-9.18225200e-02  1.00817467e+00 -3.24425081e+02]]
+    # H3 = np.array([[ 8.66225987e-01,  1.23513301e-01,  1.00071255e+04], [-9.18225200e-02,  1.00817467e+00, -3.24425081e+02]])
+    H3 = np.array([[ 8.66225987e-01,  1.23513301e-01,  1.00071255e+04], [0,  1,  0]])
+    panorama = applyTransform(panorama, image4, H3)
 
-    # THIRD STITCH
-    # Find Key Points & Calculate Transform
-    src_pts, dst_pts = findKeyPoints(panorama, image4)
-    calculatedAffine = calculateTransform(dst_pts, src_pts)
-
-    # Compare default and calculated transform
-    frobenius_norm = np.linalg.norm(calculatedAffine - defaultAffine, 'fro')
-    
-    # Use the most suitable transform
-    # if frobenius_norm < 300:
-    if True:
-        panorama = applyTransform(panorama, image4, calculatedAffine)
-    else:
-        panorama = applyTransform(panorama, image4, defaultAffine)
+    # Crop image
+    panorama = panorama[160:1830, :, :]
 
     return panorama
 
 
 if __name__ == "__main__":
-    image1 = cv2.imread('./test_images/4/2.JPG')
-    image2 = cv2.imread('./test_images/4/3.JPG')
-    image3 = cv2.imread('./test_images/4/4.JPG')
-    image4 = cv2.imread('./test_images/4/1.JPG')
+    image1 = cv2.imread('./test_images/2/cap_0.jpg')
+    image2 = cv2.imread('./test_images/2/cap_1.jpg')
+    image3 = cv2.imread('./test_images/2/cap_2.jpg')
+    image4 = cv2.imread('./test_images/2/cap_3.jpg')
 
     panorama = performPanoramicStitching(image1, image2, image3, image4)
     showImage(panorama)
