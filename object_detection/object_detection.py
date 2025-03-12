836cb977ce731e0739002f49b70ad7e6092775f1
Ross <r.inglis617@gmail.com>
2025-03-12 14:09:08 +0000

    created xyxy_to_xywh function

--- object_detection/object_detection.py
+++ object_detection/object_detection.py
@@ -24,6 +24,36 @@ def object_detection(model, frame,conf=0.25):
     print(f"Object detection results:\n{results}")
     return results
 
+def xyxy_to_xywh(coords, img_width=None, img_height=None, centre_relative=False):
+    """
+    Convert bounding box coordinates from [x1, y1, x2, y2] to [x, y, w, h].
+    With option to have [x,y] relative to centre of image
+
+    Inputs:
+        -coords (list): Bounding box in [x1, y1, x2, y2] format.
+        -img_width (int, optional): Width of the image (required if center_relative=True).
+        -img_height (int, optional): Height of the image (required if center_relative=True).
+        -center_relative (bool): If True, (x, y) will be relative to the center of the image.
+
+    Outputs:
+        tuple: Converted bounding box in [x, y, w, h] format
+    """
+    
+    x1, y1, x2, y2 = coords
+    w = int(x2 - x1)
+    h = int(y2 - y1)
+    x = int(round(x1 + w / 2))
+    y = int(round(y1 + h / 2))
+
+    if centre_relative:
+        if img_width is None or img_height is None:
+            raise ValueError("Image width and height are required when center_relative is True.")
+        x -= img_width / 2
+        y -= img_height / 2
+
+    return (x, y, w, h)
+
+
 def pixel_to_angle(pixel,res,fov):
     """Takes in pixel and returns angle from centre. Assumes linear FOV.
     pixel: (x,y) pixel location to caluclate angle to  
@@ -70,6 +100,17 @@ if __name__ == "__main__":
     img = cv2.imread("dog.jpg")
     results = object_detection(model,img)
     print(results)
+
+    # Draw bounding box
+    # bbox_img = img.copy()
+    # cv2.rectangle(bbox_img, results[0][1][:2], results[0][1][2:], (0,0,255), 2)
+    # cv2.imwrite("bbox_img.jpg",bbox_img)
+
+    # Test xyxy_to_xywh
+    box = (int(img.shape[0]/2)-50,int(img.shape[1]/2)-50,int(img.shape[0]/2)+50,int(img.shape[1]/2)+50)
+    print(box)
+    bbox = xyxy_to_xywh(box,img.shape[0],img.shape[1],True)
+    print(bbox)
 
     print(pixel_to_angle((4600,0),(4608,2592),(102,67)))
 
