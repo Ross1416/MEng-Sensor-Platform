9cf0c4552cc4002dd28354b8069825b6efedf2d0
Chris Abiaad <92934215+chrisA777@users.noreply.github.com>
2025-03-24 21:39:43 +0000

    ENABLE_DEBUG

--- PiA.py
+++ PiA.py
@@ -6,13 +6,14 @@ from time import sleep
 from comms.updateJSON import *
 import cv2
 from cameras import *
-import logging 
+import logging
 from stitching.stitching_main import performPanoramicStitching
 import json
 import shutil
 
+
 # Triggers when change in GPS location
-def new_scan(rgb_model, activeFile, lon=55.3, lat=-4,privacy=False):
+def new_scan(rgb_model, activeFile, lon=55.3, lat=-4, privacy=False):
     # Captures two images
     frames = capture(cams, "PiA")
     # Triggers capture on PiB
@@ -20,20 +21,33 @@ def new_scan(rgb_model, activeFile, lon=55.3, lat=-4,privacy=False):
     # Perform object detection
     objects = []
     for f in frames:
-        objects.append(object_detection(rgb_model,f))
+        objects.append(object_detection(rgb_model, f))
     # Retrieve slave images and data
     frames += receive_image_arrays(conn)
+
+    if ENABLE_DEBUG:
+        debug_dir = "./debug_PiA/"
+        os.makedirs(debug_dir, exist_ok=True)
+
+        for i, frame in enumerate(frames):
+            frame_id = len(
+                [f for f in os.listdir(debug_dir) if f.endswith(".jpg")]
+            )
+            cv2.imwrite(
+                os.path.join(debug_dir, f"frame_{frame_id}.jpg"), frame
+            )
+
     # Send object detection results to PiB
-    send_object_detection_results(conn, objects)  
+    send_object_detection_results(conn, objects)
     # Receive object detection data
     objects += receive_object_detection_results(conn)
     # Assign IDs to objects
     objects = assign_id(objects)
-    
-    # Blur people if privacy 
+
+    # Blur people if privacy
     if privacy:
         for i in range(len(frames)):
-            frames[i] = blur_people(frames[i],objects[i],255)
+            frames[i] = blur_people(frames[i], objects[i], 255)
 
     # Perform pano stitching
     panorama, objects = performPanoramicStitching(frames, objects)
@@ -41,14 +55,16 @@ def new_scan(rgb_model, activeFile, lon=55.3, lat=-4,privacy=False):
     objects_restructured = []
     for frame in objects:
         objects_restructured += frame
-    
+
     # Remove duplicate object detections
     filtered_objects = non_maximum_suppression(objects_restructured)
 
     # Updates json and moves images to correct folder
-    uid = str(lon)+str(lat)
+    uid = str(lon) + str(lat)
     for i in range(len(filtered_objects)):
-        filtered_objects[i][1] = xyxy_to_xywh(filtered_objects[i][1], panorama.shape[1], panorama.shape[0], True)
+        filtered_objects[i][1] = xyxy_to_xywh(
+            filtered_objects[i][1], panorama.shape[1], panorama.shape[0], True
+        )
 
     updateJSON(uid, lon, lat, filtered_objects, panorama, activeFile)
 
@@ -76,15 +92,15 @@ def new_scan(rgb_model, activeFile, lon=55.3, lat=-4,privacy=False):
     for scan in files:
         path = os.path.join(path, scan)
         if os.path.isfile(path) and scan.endswith(".png"):
-            loc = scan.find('_')+1
-            id = scan[loc:loc+2]
+            loc = scan.find("_") + 1
+            id = scan[loc : loc + 2]
             # Check if id of hyperspectral scan is in filtered objects
             if id in ids:
                 if "ndvi" in path:
                     save_path = f"./user-interface/public/images/{activeFile[:-5]}/{uid}/hs_{uid}_{id}.jpg"
                     shutil.move(path, save_path)
                     hs_classification.append(f"./{uid}/hs_{uid}_{id}.jpg")
-                else:  
+                else:
                     save_path = f"./user-interface/public/images/{activeFile[:-5]}/{uid}/hs_{uid}_{id}_ndvi.jpg"
                     shutil.move(path, save_path)
                     hs_ndvi.append(f"./{uid}/hs_{uid}_{id}_ndvi.jpg")
@@ -93,14 +109,19 @@ def new_scan(rgb_model, activeFile, lon=55.3, lat=-4,privacy=False):
     delete_files_in_dir(HSI_SCANS_PATH)
 
     # Update JSON with hyperspectral data
-    updateJSON_HS(hs_classification, hs_ndvi, hs_materials, lon, lat, activeFile)
+    updateJSON_HS(
+        hs_classification, hs_ndvi, hs_materials, lon, lat, activeFile
+    )
+
 
 PORT = 5002
-HOST = "0.0.0.0" # i.e. listening
-RESOLUTION = (4608,2592)
-FOV = (102,67)
-PRIVACY = True  #Blur people
-CLASSES = ["person"] 
+HOST = "0.0.0.0"  # i.e. listening
+RESOLUTION = (4608, 2592)
+FOV = (102, 67)
+PRIVACY = True  # Blur people
+CLASSES = ["person"]
+
+ENABLE_DEBUG = False
 
 HSI_SCANS_PATH = "./hsi_scans/"
 
@@ -108,11 +129,11 @@ if __name__ == "__main__":
     # Setup Logging
     logging.basicConfig(
         level=logging.DEBUG,
-        format="{asctime} - {levelname} - {name}: {message}",style="{",datefmt="%Y-%m-%d %H:%M",
-        handlers=[
-        logging.FileHandler("piA.log"),
-        logging.StreamHandler()
-        ])
+        format="{asctime} - {levelname} - {name}: {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M",
+        handlers=[logging.FileHandler("piA.log"), logging.StreamHandler()],
+    )
     logging.info("##### Start up new sesson. #####")
     # Setup cameras and GPIO
     cams = setup_cameras()
@@ -127,23 +148,23 @@ if __name__ == "__main__":
     logging.info(f"Set YOLO classes to {CLASSES}.")
     logging.info(f"Privacy set {PRIVACY}.")
     logging.info(f"Waiting for trigger...")
-    
-    #Mainloop
+
+    # Mainloop
     while True:
         # TODO: Check for location change
         # Update save location
         try:
             status, activeFile = getPlatformStatus()
         except json.decoder.JSONDecodeError:
-            logging.error("Error accessing JSON configuration file.")    
+            logging.error("Error accessing JSON configuration file.")
         GPS_coordinate_change = True
 
         if status == 2 or (status == 1 and GPS_coordinate_change):
             timestamp = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
             save_location = f"./capture/{timestamp}-capture/"
 
-            new_scan(rgb_model,activeFile, privacy=PRIVACY)       
-        
+            new_scan(rgb_model, activeFile, privacy=PRIVACY)
+
             logging.info("Completed scan.")
 
             if status == 2:
--- PiB.py
+++ PiB.py
@@ -1,6 +1,6 @@
 from object_detection.object_detection import *
 from comms.send import *
-from comms.receive import * 
+from comms.receive import *
 from cameras import *
 from time import sleep
 import logging
@@ -9,64 +9,92 @@ from hyperspectral.hyperspectral_driver import *
 from hyperspectral.classification import *
 import time
 
-def on_trigger(rgb_model,axis,hs_cam,cal_arr):
+
+def on_trigger(rgb_model, axis, hs_cam, cal_arr):
     # Capture images
     frames = capture(cams, "PiB", PATH)
     # Perform object detection
     objects = []
     for f in frames:
-        objects.append(object_detection(rgb_model,f))
+        objects.append(object_detection(rgb_model, f))
     # Send images to PiA
-    send_image_arrays(client_socket,frames)
+    send_image_arrays(client_socket, frames)
     # Receive object detection data
     detection_data = receive_object_detection_results(client_socket)
     objects = detection_data + objects
     # Send object detection results to PiA
-    send_object_detection_results(client_socket, objects[2:])  
+    send_object_detection_results(client_socket, objects[2:])
     # Assign IDs to objects
     objects = assign_id(objects)
 
-    # Take hyperspectral scan 
-    for i in range(len(objects)):# For every camera
+    # Take hyperspectral scan
+    for i in range(len(objects)):  # For every camera
         if objects[i] != None:
-            for j in range(len(objects[i])):# for every object detected in frame
+            for j in range(
+                len(objects[i])
+            ):  # for every object detected in frame
                 # Get corner pixels of objects detected and convert to angle
-                px_1,px_2 = objects[i][j][1][:2],objects[i][j][1][2:]
-                xoffset = i*90
-                angle_x1 = pixel_to_angle(px_1,RESOLUTION,FOV)[0] + xoffset + ROTATION_OFFSET 
-                angle_x2 = pixel_to_angle(px_2,RESOLUTION,FOV)[0] + xoffset + ROTATION_OFFSET
+                px_1, px_2 = objects[i][j][1][:2], objects[i][j][1][2:]
+                xoffset = i * 90
+                angle_x1 = (
+                    pixel_to_angle(px_1, RESOLUTION, FOV)[0]
+                    + xoffset
+                    + ROTATION_OFFSET
+                )
+                angle_x2 = (
+                    pixel_to_angle(px_2, RESOLUTION, FOV)[0]
+                    + xoffset
+                    + ROTATION_OFFSET
+                )
                 id = objects[i][j][3]
-                logging.debug(f"Camera {i}, object {j}, ID: {id}, X pixel coords: {px_1},{px_2} => X angle: {angle_x1},{angle_x2}")
+                logging.debug(
+                    f"Camera {i}, object {j}, ID: {id}, X pixel coords: {px_1},{px_2} => X angle: {angle_x1},{angle_x2}"
+                )
 
                 if ENABLE_HS:
-                    mats = on_rotate(axis,(angle_x1,angle_x2),hs_cam,cal_arr, id)
+                    mats = on_rotate(
+                        axis, (angle_x1, angle_x2), hs_cam, cal_arr, id
+                    )
                     send_images(client_socket, HSI_SCANS_PATH)
                     send_object_detection_results(client_socket, mats)
                     delete_files_in_dir(HSI_SCANS_PATH)
 
-                    
     # # Send processed hyperspectral scans to PiA
-    # send_images(client_socket, HSI_SCANS_PATH)            
+    # send_images(client_socket, HSI_SCANS_PATH)
     # # Send hyperspectral material distribution data to PiA
     # send_object_detection_results(client_socket, hs_materials)
     # # Remove all old hs scans
     # delete_files_in_dir(HSI_SCANS_PATH)
-                
 
-def on_rotate(axis,angles,hs_cam,cal_arr, id):
-    # Rotate rotational stage 
-    axis.move_absolute(angles[0],Units.ANGLE_DEGREES,velocity=80,velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,wait_until_idle=True)
+
+def on_rotate(axis, angles, hs_cam, cal_arr, id):
+    # Rotate rotational stage
+    axis.move_absolute(
+        angles[0],
+        Units.ANGLE_DEGREES,
+        velocity=80,
+        velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,
+        wait_until_idle=True,
+    )
     logging.info(f"Rotating hyperspectral to {angles[0]} degrees.")
     # Grab hyperspectral data
     fps = hs_cam.ResultingFrameRateAbs.Value
     logging.debug(f"Calculated FPS: {fps}")
     # Calculate number of frames
-    nframes = get_nframes(abs(angles[1]-angles[0]), HS_PIXEL_BINNING)
+    nframes = get_nframes(abs(angles[1] - angles[0]), HS_PIXEL_BINNING)
     logging.debug(f"Will grab {nframes} frames.")
-    speed = get_rotation_speed(nframes,fps,abs(angles[1]-angles[0]))
+    speed = get_rotation_speed(nframes, fps, abs(angles[1] - angles[0]))
     logging.info("Grabbing hyperspectral scan...")
-    axis.move_absolute(angles[1],Units.ANGLE_DEGREES,velocity=speed,velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,wait_until_idle=False) # temporarily blocking
-    scene = grab_hyperspectral_scene(hs_cam, nframes, None, None,"test",calibrate=False)
+    axis.move_absolute(
+        angles[1],
+        Units.ANGLE_DEGREES,
+        velocity=speed,
+        velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,
+        wait_until_idle=False,
+    )  # temporarily blocking
+    scene = grab_hyperspectral_scene(
+        hs_cam, nframes, None, None, "test", calibrate=False
+    )
     # Plot RGB image for test
     # print("Plotting RGB Image...")
     # plt.figure()
@@ -78,30 +106,49 @@ def on_rotate(axis,angles,hs_cam,cal_arr, id):
     # )
 
     # plt.imshow(scene[:, :, RGB])
-    output_path = HSI_SCANS_PATH+f"hs_{id}.jpg"
-    mats = classify_and_save(MODEL_PATH, scene, LABEL_ENCODING_PATH, output_path)
+
+    # Save scene as .npy if debugging
+    if ENABLE_DEBUG:
+        debug_dir = "./debug_PiB/"
+        os.makedirs(debug_dir, exist_ok=True)
+        next_id = len(
+            [
+                f
+                for f in os.listdir(debug_dir)
+                if f.startswith("scene_") and f.endswith(".npy")
+            ]
+        )
+        path = os.path.join(debug_dir, f"scene_{next_id}.npy")
+        np.save(path, scene)
+        logging.debug(f"Saved raw hyperspectral scene to {path}")
+
+    output_path = HSI_SCANS_PATH + f"hs_{id}.jpg"
+    mats = classify_and_save(
+        MODEL_PATH, scene, LABEL_ENCODING_PATH, output_path
+    )
     print(mats)
 
     return mats
 
 
-
-
 # IP = "hsiA.local"
 
 ENABLE_HS = False
+ENABLE_DEBUG = False
 
 IP = "10.42.0.1"
 PORT = 5002
 PATH = "./captures/"
-CLASSES = ["person"] 
-ROTATIONAL_STAGE_PORT = "/dev/ttyUSB0" # TODO: find automatically?
-ROTATION_OFFSET = 20  # temporary 
+CLASSES = ["person"]
+ROTATIONAL_STAGE_PORT = "/dev/ttyUSB0"  # TODO: find automatically?
+ROTATION_OFFSET = 20  # temporary
 
-RESOLUTION = (4608,2592)
-FOV = (102,67)
+RESOLUTION = (4608, 2592)
+FOV = (102, 67)
 
-CALIBRATION_FILE_PATH = "./hyperspectral/calibration/BaslerPIA1600_CalibrationA.txt"
+CALIBRATION_FILE_PATH = (
+    "./hyperspectral/calibration/BaslerPIA1600_CalibrationA.txt"
+)
 
 
 MODEL_PATH = "./hyperspectral/NN_18_03_2025.keras"
@@ -110,36 +157,43 @@ HSI_SCANS_PATH = "./hsi_scans/"
 
 HS_EXPOSURE_TIME = 10000
 HS_PIXEL_BINNING = 2
-HS_GAIN = 200 
+HS_GAIN = 200
 
 if __name__ == "__main__":
     # Setup Logging
     logging.basicConfig(
         level=logging.DEBUG,
-        format="{asctime} - {levelname} - {name}: {message}",style="{",datefmt="%Y-%m-%d %H:%M",
-        handlers=[
-        logging.FileHandler("piB.log"),
-        logging.StreamHandler()
-        ])
+        format="{asctime} - {levelname} - {name}: {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M",
+        handlers=[logging.FileHandler("piB.log"), logging.StreamHandler()],
+    )
     logging.info("##### Start up new sesson. #####")
     try:
         # Setup cameras and capture images
         cams = setup_cameras()
         logging.debug("Setup cameras.")
-        
-        
+
         if ENABLE_HS:
             # Setup rotational stage
             zaber_conn, axis = setup_zaber(ROTATIONAL_STAGE_PORT)
             logging.debug("Setup rotational stage.")
-    
+
             # Home rotational stage
             logging.info("Homing rotational stage.")
             axis.home(wait_until_idle=True)
-            axis.move_absolute(ROTATION_OFFSET,Units.ANGLE_DEGREES,velocity=80,velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,wait_until_idle=True) # temporarily blocking
+            axis.move_absolute(
+                ROTATION_OFFSET,
+                Units.ANGLE_DEGREES,
+                velocity=80,
+                velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,
+                wait_until_idle=True,
+            )  # temporarily blocking
 
             # Setup hyperspectral
-            hs_cam = setup_hyperspectral(HS_EXPOSURE_TIME, HS_GAIN, HS_PIXEL_BINNING)
+            hs_cam = setup_hyperspectral(
+                HS_EXPOSURE_TIME, HS_GAIN, HS_PIXEL_BINNING
+            )
             logging.info("Setup hyperspectral camera.")
 
         else:
@@ -151,30 +205,38 @@ if __name__ == "__main__":
         # Make connection with PiA
         client_socket = make_client_connection(IP, PORT)
         logging.debug("Connected to PiA")
-        
+
         # Setup object detection modelx
-        rgb_model = YOLOWorld("object_detection/yolo_models/yolov8s-worldv2.pt")
+        rgb_model = YOLOWorld(
+            "object_detection/yolo_models/yolov8s-worldv2.pt"
+        )
         logging.debug("Loaded RGB object detection model.")
         # TODO send search classes to PiB
         rgb_model.set_classes(CLASSES)
         logging.info(f"Set YOLO classes to {CLASSES}.")
-        
+
         logging.info("Setup complete. Waiting to start capture...")
 
         # Poll for trigger capture signal
         while True:
             if receive_capture_request(client_socket) == 1:
                 logging.info("Triggered Capture.")
-                on_trigger(rgb_model,axis,hs_cam,cal_arr)
+                on_trigger(rgb_model, axis, hs_cam, cal_arr)
                 capture_triggered = True
-            
+
             sleep(1)
-    
+
     except Exception as e:
         logging.error(f"Error in PiB.py: {e}")
 
         if ENABLE_HS:
-            axis.move_absolute(2,Units.ANGLE_DEGREES,velocity=80,velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,wait_until_idle=True) # temporarily blocking
+            axis.move_absolute(
+                2,
+                Units.ANGLE_DEGREES,
+                velocity=80,
+                velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,
+                wait_until_idle=True,
+            )  # temporarily blocking
             hs_cam.Close()
             zaber_conn.close()
         client_socket.close()
@@ -182,12 +244,15 @@ if __name__ == "__main__":
 
     finally:
         if ENABLE_HS:
-            axis.move_absolute(2,Units.ANGLE_DEGREES,velocity=80,velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,wait_until_idle=True) # temporarily blocking
+            axis.move_absolute(
+                2,
+                Units.ANGLE_DEGREES,
+                velocity=80,
+                velocity_unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND,
+                wait_until_idle=True,
+            )  # temporarily blocking
             hs_cam.Close()
             zaber_conn.close()
         client_socket.close()
         logging.info("All connections closed.")
         logging.info("System terminated.")
-    
-
-
